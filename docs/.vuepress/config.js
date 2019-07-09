e98c8fb6bcb03abffc24619e54d209d5678df2b8
cobainby <619148077@qq.com>
2019-07-10 01:10:55 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'Stone\'s blog',  //博客标题
+    title: 'geek by',  //博客标题
     description: '我的个人博客', //个人描述
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', {  // 增加一个自定义的 favicon(网页标签的图标)
@@ -16,64 +16,48 @@ module.exports = {
     themeConfig: {
       nav: [ // 导航栏配置
         {
-          text: 'Home',
+          text: '首页',
           link: '/' // '/'默认为docs文件夹下的README.md文件
 
         },
         {
-          text: 'Blog',
-          link: '/blog/'
+          text: '前端daily',
+          link: '/webdaily/' // '/'默认为docs文件夹下的README.md文件
+
         },
         {
-          text: 'Space',
+          text: '疑惑困难',
+          link: '/problems/' // '/'默认为docs文件夹下的README.md文件
+
+        },
+        {
+          text: '工具收藏',
+          link: '/tools/' // '/'默认为docs文件夹下的README.md文件
+
+        },
+        {
+          text: '随笔',
           link: '/space/'
         },
         {
           text: 'GitHub',
-          link: 'https://github.com/HUYIJUNCODING'
+          link: 'https://github.com/cobainby'
         }
       ],
       sidebar: {
-        '/blog/': [
-          {
-            title: '前端daily', //侧边栏名称
-            collapsable: true, // 可折叠
-            children: [
-                '/blog/javaScript/one', //文章地址
-                '/blog/javaScript/two',
-            ]
-          },
-          {
-            title: '框架相关',
-            collapsable: true,
-            children: [
-                '/blog/framework/vuepressBlog',
-            ]
-          },
-          {
-            title: '工具收藏',
-            collapsable: true,
-            children: [
-                '/blog/tools/one',
-                '/blog/tools/two',
-            ]
-          },
+        '/webdaily/':[
+          ''
         ],
-        '/space/': [
-          {
-            title: '诗和远方',
-            collapsable: true,
-            children: [
-                '/space/poetryAndDistance/one',
-            ]
-          },
-          {
-            title: '谝闲传',
-            collapsable: true,
-            children: [
-                '/space/talk/one',
-            ]
-          },
+        '/problems/':[
+          '',
+          '解决vue项目打包路径错误'
+        ],
+        '/tools/':[
+          ''
+        ],
+        '/space/':[
+          '',
+          'blog的重新出发'
         ]
       }, // 侧边栏配置
       sidebarDepth: 2, // 侧边栏显示2级
---
+++ docs/problems/解决vue项目打包路径错误.md
@@ -0,0 +1,19 @@
+# 解决Vue项目打包路径错误的问题
+---
+在Vue项目打包的过程中，由于图片路径不一致，造成打包后无法访问到图片的资源。
+
+### 解决办法：
+*更改build/utils.js文件中的ExtractTextPlugin插件的options相关配置*
+
+```
+if(option.extract){
+  return ExtractTextPlugin.extract({
+    use:loaders,
+    publicPath:'../../', //注意配置这一部分，根据目录结构自由调整
+    fallback:'vue-style-loader'
+   })
+}else{
+  return['vue-style-loader'].concat(loaders)
+}
+
+```
